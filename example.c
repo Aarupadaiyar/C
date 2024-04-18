ea6fe2b4dc8844a98c5814bdc0eeaf1b6f12d3d0
Aarupadaiyar <143502182+Aarupadaiyar@users.noreply.github.com>
2024-04-18 10:12:15 +0530

    ..

--- example.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int add(int x , int y){
-int a;
-a=x+y;
-printf("%d",a);
-}
-int main(){
-add(9,8);
-}
---
+++ week1/example.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+short int s;
+int x = 99999;
+s = x;
+printf("%d\n", s);
+}
