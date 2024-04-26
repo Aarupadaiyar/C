365ed84f60b8cbd2fdb5c339172d3f65587ba7db
Aarupadaiyar <143502182+Aarupadaiyar@users.noreply.github.com>
2024-04-26 23:03:55 +0530

    .

---
+++ Week 2/variable.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+    char name[]="aarov";
+    int age = 19;
+    printf("there was once a revolutionary man named %s",name);
+    printf("He was just %d years old",age);
+}
